84f9dc8e45f8955eec5462507e4e4f4806321e2e
Piotr Górski <gorski.piotr.jan@gmail.com>
2016-09-17 08:51:02 +0200

    final solution assignment 1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,13 +9,27 @@ $scope.message ="";
 $scope.inputValue ="";
 
 $scope.checkIfTooMuch = function() {
-  var stringToSplit = $scope.inputValue;
-  var number = stringToSplit.split(',');
-  if (number.length <=3)
-    $scope.message = "Enjoy!";
 
-  if (number.length >3)
-    $scope.message = "Too much!";
+
+  var stringToSplit = $scope.inputValue;
+  if(stringToSplit.length == 0)
+    $scope.message = "Please enter data first!";
+  else
+  {
+    var array = stringToSplit.split(',');
+
+    var finalNumber = 0;
+    for(var i=0; i<array.length;i++)
+    {
+      if(array[i].length > 0)
+        finalNumber++;
+    }
+    if (finalNumber <=3)
+      $scope.message = "Enjoy!";
+
+    if (finalNumber >3)
+      $scope.message = "Too much!";
+  }
 
 
 };
