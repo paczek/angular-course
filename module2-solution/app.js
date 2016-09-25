ce504d5dfc666574661453532ec40390a214a014
Piotr Górski <pgorski@abis.krakow.pl>
2016-09-25 15:48:36 +0200

    minor changes

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -9,20 +9,18 @@ angular.module('ShoppingListCheckOff', [])
 
 AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService',];
 function AlreadyBoughtShoppingController(ShoppingListCheckOffService){
-var boughtList = this;
-boughtList.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+	var boughtList = this;
+	boughtList.boughtItems = ShoppingListCheckOffService.getBoughtItems();
 }
 
 ToBuyShoppingController.$inject = ['ShoppingListCheckOffService',];
 function ToBuyShoppingController(ShoppingListCheckOffService){
-var toBuyList = this;
-toBuyList.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
+	var toBuyList = this;
+	toBuyList.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
 
-toBuyList.buyItem = function (itemIndex, itemName, itemQuantity) {
-    ShoppingListCheckOffService.buyItem(itemIndex, itemName, itemQuantity);
-  };
-  
-  
+	toBuyList.buyItem = function (itemIndex, itemName, itemQuantity) {
+		ShoppingListCheckOffService.buyItem(itemIndex, itemName, itemQuantity);
+	};
 }
 
 function ShoppingListCheckOffService() {
@@ -32,14 +30,14 @@ function ShoppingListCheckOffService() {
   var itemsToBuy = [{name: "Tomatoes", quantity: "2"}, {name: "Potatoes", quantity: "5"}, {name: "Iron mans", quantity: "3"}, {name: "Chaos bringer", quantity: "6"},{name: "Steam accounts", quantity: "2"},];
   var itemsBought = [];
 
-  service.buyItem = function (itemIndex, itemName, quantity) {
-    itemsToBuy.splice(itemIndex, 1);
-	 var item = {
-     name: itemName,
-     quantity: quantity
-     };
-     itemsBought.push(item);
-  };
+	service.buyItem = function (itemIndex, itemName, quantity) {
+		itemsToBuy.splice(itemIndex, 1);
+		var item = {
+			name: itemName,
+			quantity: quantity
+		};
+		itemsBought.push(item);
+	};
   
   service.getToBuyItems = function () {
     return itemsToBuy;
