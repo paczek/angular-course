700b998867b6a059604b8cc08738377d8b26672b
Piotr Górski <pgorski@abis.krakow.pl>
2016-09-25 15:04:07 +0200

    pre-final draft

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,37 +1,53 @@
 (function () {
   'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
-LunchCheckController.$inject = ['$scope','$filter'];
-function LunchCheckController($scope, $filter){
-$scope.message ="";
-$scope.inputValue ="";
-
-$scope.checkIfTooMuch = function() {
-  var stringToSplit = $scope.inputValue;
-  if(stringToSplit.length == 0)
-    $scope.message = "Please enter data first!";
-  else
-  {
-    var array = stringToSplit.split(',');
-
-    var finalNumber = 0;
-    for(var i=0; i<array.length;i++)
-    {
-      if(array[i].length > 0)
-        finalNumber++;
-    }
-    if (finalNumber <=3)
-      $scope.message = "Enjoy!";
-
-    if (finalNumber >3)
-      $scope.message = "Too much!";
-  }
+angular.module('ShoppingListCheckOff', [])
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 
-};
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService',];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService){
+var boughtList = this;
+boughtList.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+}
 
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService',];
+function ToBuyShoppingController(ShoppingListCheckOffService){
+var toBuyList = this;
+toBuyList.toBuyItems = ShoppingListCheckOffService.getToBuyItems();
+
+toBuyList.buyItem = function (itemIndex, itemName, itemQuantity) {
+    ShoppingListCheckOffService.buyItem(itemIndex, itemName, itemQuantity);
+  };
+  
+  
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var itemsToBuy = [{name: "Tomatoes", quantity: "2"}, {name: "Potatoes", quantity: "5"}, {name: "Iron mans", quantity: "3"}, {name: "Chaos bringer", quantity: "6"},{name: "Steam accounts", quantity: "2"},];
+  var itemsBought = [];
+
+  service.buyItem = function (itemIndex, itemName, quantity) {
+    itemsToBuy.splice(itemIndex, 1);
+	 var item = {
+     name: itemName,
+     quantity: quantity
+     };
+     itemsBought.push(item);
+  };
+  
+  service.getToBuyItems = function () {
+    return itemsToBuy;
+  };
+  
+  service.getBoughtItems = function () {
+    return itemsBought;
+  };
 }
 
 })();
